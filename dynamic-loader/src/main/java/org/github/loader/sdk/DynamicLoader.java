c8a9bf88e2deed4e57a19564edd4b5b96d9ac263
yum <jobni@163.com>
2017-06-20 14:33:43 +0800

    添加了jar version

--- dynamic-loader/src/main/java/org/github/loader/sdk/DynamicLoader.java
+++ dynamic-loader/src/main/java/org/github/loader/sdk/DynamicLoader.java
@@ -40,9 +40,10 @@ public class DynamicLoader implements IDynamicLoader {
      * 170 支持A加载B独立协议
      * 200 修改了新包名包名,新插件preference配置修改
      * 202 兼容老版本
+     * 203 下载B协议增加jar_version
      */
     //此版本号,在每次修改jar的代码的时候,需要更新并记录修改日志.每次涨10.
-    public static final int JAR_VERSION =202;
+    public static final int JAR_VERSION =203;
 
     //当前使用的dex路径
     private final String operationPath;
